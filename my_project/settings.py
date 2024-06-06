d6edd958718678c3f6167374588cbcdece3651b5
bugston <bugstonn@gmail.com>
2024-06-06 12:17:50 +0300

    working admin text fileds

--- my_project/settings.py
+++ my_project/settings.py
@@ -20,12 +20,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-f6qtk-sovvg3_%c2(tc8h3_^8wog+(f(_r4iixutidivuci(@u'
+SECRET_KEY = 'django-insecure-5==29@706m&5rygp(*xn5+80(-h077bvcl4kn@v5w42@l%!u!9'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = []
 
 
 # Application definition
@@ -37,8 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'myapp',
-    'corsheaders',
+    'myapp',   
 ]
 
 MIDDLEWARE = [
@@ -50,8 +49,6 @@ MIDDLEWARE = [
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
-
-
 
 ROOT_URLCONF = 'my_project.urls'
 
@@ -72,8 +69,6 @@ TEMPLATES = [
 ]
 
 WSGI_APPLICATION = 'my_project.wsgi.application'
-X_FRAME_OPTIONS = 'ALLOWALL'
-X_FRAME_OPTIONS = 'SAMEORIGIN'
 
 
 # Database
@@ -122,7 +117,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 STATIC_URL = 'static/'
-STATIC_ROOT = BASE_DIR / 'static'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
--- myapp/admin.py
+++ myapp/admin.py
@@ -1,6 +1,20 @@
 from django.contrib import admin
-from .models import sections
+from .models import Sections
+from django.http import HttpResponseRedirect
+from .models import Villain
 
-@admin.register(sections)
-class sectionsAdmin(admin.ModelAdmin):
-    list_display = ('html', 'css', 'js')
+admin.site.register(Sections)
+
+@admin.register(Villain)
+class VillainAdmin(admin.ModelAdmin):
+    change_form_template = "admin/your_app/change_form.html"
+
+    def response_change(self, request, obj):
+        if "_make-unique" in request.POST:
+            matching_names_except_this = self.get_queryset(request).filter(name=obj.name).exclude(pk=obj.id)
+            matching_names_except_this.delete()
+            obj.is_unique = True
+            obj.save()
+            self.message_user(request, "This villain is now unique")
+            return HttpResponseRedirect(".")
+        return super().response_change(request, obj)
