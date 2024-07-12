bef91458e9fbfd265e1e6e323e8ca6d8eee8c9ea
bugston <bugstonn@gmail.com>
2024-07-12 11:37:52 +0300

    s

--- model_fields/views.py
+++ model_fields/views.py
@@ -27,8 +27,8 @@ def create_instance(request):
             email_field="example@example.com",
             url_field="https://example.com",
             uuid_field="550e8400-e29b-41d4-a716-446655440000",
-            file_field="path/to/uploaded_file.txt",
-            image_field="path/to/uploaded_image.jpg",
+            file_field="model_fields/templates/",
+            image_field="model_fields/templates/",
             json_data={"key": "value"}
         )
         instance.save()
