efe51cfb8bfcfeafe12c64678876f9b392c215fb
bugston <bugstonn@gmail.com>
2024-07-16 13:56:38 +0300

    model now stores file name and path to json in db and remembers it when a field is modified in a saved model

--- model_fields/forms.py
+++ model_fields/forms.py
@@ -4,7 +4,7 @@ from .models import AllFieldsModel
 class AllFieldsModelForm(forms.ModelForm):
     class Meta:
         model = AllFieldsModel
-        exclude = ('json_data',)
+        exclude = ('json_data',)  # Exclude json_data field from form display
 
     def __init__(self, *args, **kwargs):
         super(AllFieldsModelForm, self).__init__(*args, **kwargs)
--- model_fields/models.py
+++ model_fields/models.py
@@ -1,10 +1,11 @@
 from django.db import models
 from ckeditor.fields import RichTextField
+from django.conf import settings
+import os
 
 class AllFieldsModel(models.Model):
-
     bool_field = models.BooleanField(default=False)
-    json_data = models.JSONField()
+    json_data = models.JSONField(default=dict)  # Ensure json_data defaults to an empty dictionary
     null_bool_field = models.BooleanField(null=True, blank=True)
     char_field = models.CharField(max_length=100, null=True, blank=True)
     text_field = RichTextField(blank=True, null=True)
@@ -23,10 +24,24 @@ class AllFieldsModel(models.Model):
     file = models.FileField(upload_to='files/', null=True, blank=True)
 
     def save(self, *args, **kwargs):
-        self.json_data = {
-            'name': self.name,
-            'email': self.email,
-            'phone': self.phone,
-            'file': self.file.name if self.file else None,
-        }
+        original_json_data = self.json_data
         super().save(*args, **kwargs)
+        if self.file:
+            file_path = os.path.join(settings.MEDIA_URL, self.file.name)
+            file_name = os.path.basename(self.file.name)
+            self.json_data = {
+                'name': self.name,
+                'email': self.email,
+                'phone': self.phone,
+                'file': {file_name: file_path},
+            }
+        else:
+            self.json_data = {
+                'name': self.name,
+                'email': self.email,
+                'phone': self.phone,
+                'file': original_json_data.get('file', {}) if original_json_data else {},
+            }
+
+        if self.json_data != original_json_data:
+            super().save(*args, **kwargs)
