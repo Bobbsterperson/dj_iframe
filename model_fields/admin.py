32bf2c593e1a71d48c505009ca3c89187b98404a
bugston <bugstonn@gmail.com>
2024-07-30 16:25:18 +0300

    removed fieldsets

--- model_fields/admin.py
+++ model_fields/admin.py
@@ -4,6 +4,9 @@ from .forms import DynamicFieldsModelForm
 
 @admin.register(DynamicFieldsModel)
 class DynamicFieldsModelAdmin(admin.ModelAdmin):
+    """
+    Admin interface for DynamicFieldsModel, which includes dynamically generated fields.
+    """
     form = DynamicFieldsModelForm
 
     def get_fieldsets(self, request, obj=None):
@@ -12,29 +15,29 @@ class DynamicFieldsModelAdmin(admin.ModelAdmin):
         """
         json_generator = JsonFieldDefinition.objects.first()
         json_data = json_generator.json_dt if json_generator else {}
-        json_fields = [key for key in json_data.keys()]
-        fieldsets = (
-            (None, {
-                'fields': ('char_field', 'int_field', 'float_field', 'date_field', *json_fields)
-            }),
-        )
+        all_field_names = [f.name for f in DynamicFieldsModel._meta.get_fields()]
+        json_fields = []
+        for key in json_data.keys():
+            if key in all_field_names:
+                json_fields.append(key)
+        fieldsets = tuple(json_fields)
         return fieldsets
 
     def save_model(self, request, obj, form, change):
         """
         Override save_model to update json_data with dynamic fields before saving.
         """
-        json_data_fields = self.get_json_fields()
+        fieldsets = self.get_fieldsets(request)
+        json_data_fields = []
+        for fieldset in fieldsets:
+            if fieldset[0] == 'JSON Data':
+                json_data_fields.extend(fieldset[1]['fields'])
         obj.update_json_data(json_data_fields)
         super().save_model(request, obj, form, change)
 
-    def get_json_fields(self):
-        """
-        Get JSON-defined fields.
-        """
-        json_generator = JsonFieldDefinition.objects.first()
-        return json_generator.json_dt.keys() if json_generator else []
-
 @admin.register(JsonFieldDefinition)
 class JsonFieldDefinitionAdmin(admin.ModelAdmin):
+    """
+    Admin interface for JsonFieldDefinition.
+    """
     pass
