be4eff2faea258a357fe0008571fb5f85a9af247
bugston <bugstonn@gmail.com>
2024-07-30 17:39:35 +0300

    single static field generated from forms with no relation to models

--- model_fields/admin.py
+++ model_fields/admin.py
@@ -4,40 +4,11 @@ from .forms import DynamicFieldsModelForm
 
 @admin.register(DynamicFieldsModel)
 class DynamicFieldsModelAdmin(admin.ModelAdmin):
-    """
-    Admin interface for DynamicFieldsModel, which includes dynamically generated fields.
-    """
     form = DynamicFieldsModelForm
 
-    def get_fieldsets(self, request, obj=None):
-        """
-        Dynamically generate fieldsets based on JSON field definitions.
-        """
-        json_generator = JsonFieldDefinition.objects.first()
-        json_data = json_generator.json_dt if json_generator else {}
-        all_field_names = [f.name for f in DynamicFieldsModel._meta.get_fields()]
-        json_fields = []
-        for key in json_data.keys():
-            if key in all_field_names:
-                json_fields.append(key)
-        fieldsets = tuple(json_fields)
-        return fieldsets
-
     def save_model(self, request, obj, form, change):
-        """
-        Override save_model to update json_data with dynamic fields before saving.
-        """
-        fieldsets = self.get_fieldsets(request)
-        json_data_fields = []
-        for fieldset in fieldsets:
-            if fieldset[0] == 'JSON Data':
-                json_data_fields.extend(fieldset[1]['fields'])
-        obj.update_json_data(json_data_fields)
         super().save_model(request, obj, form, change)
 
 @admin.register(JsonFieldDefinition)
 class JsonFieldDefinitionAdmin(admin.ModelAdmin):
-    """
-    Admin interface for JsonFieldDefinition.
-    """
     pass
--- model_fields/forms.py
+++ model_fields/forms.py
@@ -1,31 +1,7 @@
 from django import forms
 from .models import DynamicFieldsModel, JsonFieldDefinition
 
-class DynamicFieldsModelForm(forms.ModelForm):
-    """
-    Form to dynamically create fields based on JSON field definitions.
-    """
-    def __init__(self, *args, **kwargs):
-        super().__init__(*args, **kwargs)
-        json_generator = JsonFieldDefinition.objects.first()
-        json_data = json_generator.json_dt if json_generator else {}
-        for key, value in json_data.items():
-            if key not in [f.name for f in DynamicFieldsModel._meta.get_fields()]:
-                form_type = {
-                    'str': forms.CharField,
-                    'int': forms.IntegerField,
-                    'float': forms.FloatField,
-                    'bool': forms.BooleanField,
-                    'date': forms.DateField,
-                    'datetime': forms.DateTimeField,
-                    'decimal': forms.DecimalField,
-                }.get(value['type'], forms.CharField)
-                self.fields[key] = form_type(
-                    label=key.capitalize(),
-                    initial=value['value'],
-                    required=False
-                )
 
-    class Meta:
-        model = DynamicFieldsModel
-        exclude = ('json_data',)
+class DynamicFieldsModelForm(forms.ModelForm):
+    bool_field = forms.BooleanField(label='Boolean field', initial=False, required=False)
+
