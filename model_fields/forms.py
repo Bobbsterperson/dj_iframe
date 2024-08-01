df9df6399f79db14e00bb5b087c0d4ad5dfdccfd
bugston <bugstonn@gmail.com>
2024-08-01 10:10:49 +0300

    data saved in db from dynamicaly generated fields is now visable in the saved model and can be edited and saved

--- model_fields/forms.py
+++ model_fields/forms.py
@@ -3,49 +3,52 @@ from django import forms
 from .models import DynamicFieldsModel, JsonFieldDefinition
 
 class DynamicFieldsModelForm(forms.ModelForm):
-    # dynamic_data = forms.JSONField()  # widget=forms.HiddenInput()
-    
     class Meta:
-        exclude = ('dynamic_data',)
         model = DynamicFieldsModel
+        exclude = ('dynamic_data',)
         fields = '__all__'
-
 
     def __init__(self, *args, **kwargs):
         super(DynamicFieldsModelForm, self).__init__(*args, **kwargs)
-        
         instance = kwargs.get('instance')
         if instance and instance.foreign_key_id:
             json_fields = instance.foreign_key_id.json_field
+            dynamic_data = instance.dynamic_data or {}
             for name, field in json_fields.items():
-                if field['type'] == "str":
+                field_type = field.get('type')
+                initial_value = dynamic_data.get(name, '' if field_type == 'str' else None)
+                if field_type == "str":
                     self.fields[name] = forms.CharField(
                         required=False,
-                        widget=forms.TextInput(attrs={'placeholder': field['value']}))
-                elif field['type'] == "int":
+                        initial=initial_value,
+                        widget=forms.TextInput(attrs={'placeholder': field.get('value', '')})
+                    )
+                elif field_type == "int":
                     self.fields[name] = forms.IntegerField(
                         required=False,
-                        widget=forms.NumberInput(attrs={'placeholder': field['value']}))
-                elif field['type'] == "bool":
+                        initial=initial_value,
+                        widget=forms.NumberInput(attrs={'placeholder': field.get('value', '')})
+                    )
+                elif field_type == "bool":
                     self.fields[name] = forms.BooleanField(
                         required=False,
-                        widget=forms.CheckboxInput(attrs={'placeholder': field['value']}))
-
+                        initial=dynamic_data.get(name, False),
+                        widget=forms.CheckboxInput(attrs={'placeholder': field.get('value', '')})
+                    )
 
     def clean(self) -> dict[str, Any]:
         cleaned_data = super().clean()
         return cleaned_data
     
-    def save(self, *args, **kwargs) -> DynamicFieldsModel:
+    def save(self, commit=True, *args, **kwargs) -> DynamicFieldsModel:
+        instance = super(DynamicFieldsModelForm, self).save(commit=False, *args, **kwargs)
         dynamic_values = {}
-        json_fields_id = self["foreign_key_id"].value()
+        json_fields_id = self.instance.foreign_key_id_id
         json_fields = JsonFieldDefinition.objects.get(id=json_fields_id).json_field
-        
         for name in json_fields.keys():
-            dynamic_values[name] = self.cleaned_data.get(name)
-
-        print(dynamic_values)
-        
-        instance = super(DynamicFieldsModelForm, self).save(*args, **kwargs)
+            if name in self.cleaned_data:
+                dynamic_values[name] = self.cleaned_data[name]
         instance.dynamic_data = dynamic_values
+        if commit:
+            instance.save()
         return instance
